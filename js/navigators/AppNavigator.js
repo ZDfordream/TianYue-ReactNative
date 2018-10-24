cf0d0edf0563d4c207d33440b1ac283e8d012b67
zhudong <503749378@qq.com>
2018-10-24 21:34:41 +0800

    init

---
+++ js/navigators/AppNavigator.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {StackNavigator} from 'react-navigation';
+import WelcomePage from '../page/WelcomePage'
+import HomePage from '../page/HomePage'
+
+export default AppNavigator = StackNavigator({
+    WelcomePage: {
+        screen: WelcomePage
+    },
+    HomePage: {
+        screen: HomePage
+    }
+}, {
+    navigationOptions: {
+        header: null
+    }
+})
